1331ff77f5090515453f7c9ed96e57aee28e0515
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-03-07 23:07:20 +0300

    nav setup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Layout = () => {
     return (
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    {/* Empty navigation bar */}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;

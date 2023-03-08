cdbd88fa84f0c0107e6895389b42d3177b8c7b86
ashmalzahra <ashmal.zahraa@gmail.com>
2023-03-09 02:22:25 +0500

    Connect to API and display data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './RocketsSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    rockets: rocketsReducer,
+  },
 });

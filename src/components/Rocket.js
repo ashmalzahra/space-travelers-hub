3a7ff2606eb39bf24fd97482fe157e1dbc7d4ac5
ashmalzahra <ashmal.zahraa@gmail.com>
2023-03-10 02:45:56 +0500

    Conditional badge

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -20,9 +20,10 @@ const Rocket = ({rocket}) => {
                 <img src={rocket.flickr_images} alt={rocket.rocket_name} className="pic"/>
                 <div>
                     <h3 className="title">{rocket.rocket_name}</h3>
-                    {rocket.reserved === true && <button>Reserved</button>}
-                    <p className="description">{rocket.description}</p>
-                    {rocket.reserved === true ? (<Button variant="primary" id={rocket.rocket_id} onClick={() => cancelRocketHandler(rocket.rocket_id)}>Cancel Reservation</Button> ) :
+                    <p className="description">
+                      {rocket.reserved === true && <Button variant="success">Reserved</Button>}
+                      {rocket.description}</p>
+                    {rocket.reserved === true ? (<Button variant="light" id={rocket.rocket_id} onClick={() => cancelRocketHandler(rocket.rocket_id)}>Cancel Reservation</Button> ) :
                     (<Button variant="primary" id={rocket.rocket_id} onClick={() => reserveRocketHandler(rocket.rocket_id)}>Reserve Rocket</Button>)}
                 </div>
             </div>
